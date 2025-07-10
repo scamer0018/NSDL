1adcc8cf976faa3f06ad60581b26db42423f6164
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2025-07-10 16:40:08 +0530

    Update Hi.py

--- src/Commands/Hi.py
+++ src/Commands/Hi.py
@@ -1,19 +1,22 @@
 from libs import BaseCommand, MessageClass
 
+
 class Command(BaseCommand):
 
     def __init__(self, client, handler):
-        super().__init__(client, handler, {
-            'command': 'hi',
-            'category': 'core',
-            'description': {
-                'content': 'Say hello to the bot'
-            },
-            'exp': 1
-        })
+        super().__init__(
+            client, handler, {
+                'command': 'hi',
+                'category': 'core',
+                'description': {
+                    'content': 'Say hello to the bot'
+                },
+                'exp': 1
+            })
 
     def exec(self, M: MessageClass, contex):
         exp = self.client.db.get_user_by_jid(
-            M.sender.jid).exp if self.client.db.get_user_by_jid(M.sender.jid) else 0
+            M.sender.jid).exp if self.client.db.get_user_by_jid(
+                M.sender.jid) else 0
         self.client.reply_message(
             f"🎯 Hey *@{M.sender.jid}*! Your current EXP is: *{exp}*.", M)
